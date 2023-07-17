a1779846f2952dab8fd23ca7477cbd39ffb40622
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-07-17 23:34:15 +0100

    17 July 2023

--- src/Components/Home/HomeTwo.js
+++ src/Components/Home/HomeTwo.js
@@ -5,24 +5,136 @@ const HomeTwo = () => {
        <div className="marquee">
             <div className="marquee--inner">
                 <span className="home2span">
-                    <div className="card1"></div>
-                    <div className="card2"></div>
-                    <div className="card3"></div>
-                    <div className="card4"></div>
-                    <div className="card5"></div>
-                    <div className="card6"></div>
-                    <div className="card7"></div>
-                    <div className="card8"></div>
+                    <div className="card1">
+                        <a href="/">
+                            <h2>Coding and Programming</h2>
+                            <p className="cardText">
+                                You're a click away from the best Developers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card2">
+                        <a href="/">
+                            <h2>Graphics and Design</h2>
+                            <p className="cardText">
+                                You're a click away from the best Designers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card3">
+                        <a href="/">
+                            <h2>Video Editing and Animation</h2>
+                            <p className="cardText">
+                                You're a click away from the best Video Editors and Animators
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card4">
+                        <a href="/">
+                            <h2>Arts and Drawing</h2>
+                            <p className="cardText">
+                                You're a click away from the best Artist
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card5">
+                        <a href="/">
+                            <h2>Music and Melody</h2>
+                            <p className="cardText">
+                                You're a click away from the best musicians and Producers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card6">
+                        <a href="/">
+                            <h2>Modelling</h2>
+                            <p className="cardText">
+                                You're a click away from the best Modellers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card7">
+                        <a href="/">
+                            <h2>Architectural designs</h2>
+                            <p className="cardText">
+                                You're a click away from the best Architects
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card8">
+                        <a href="/">
+                            <h2>Influencing</h2>
+                            <p className="cardText">
+                                You're a click away from the best Influencers
+                            </p>
+                        </a>
+                    </div>
                 </span>
                 <span className="home2span">
-                    <div className="card1"></div>
-                    <div className="card2"></div>
-                    <div className="card3"></div>
-                    <div className="card4"></div>
-                    <div className="card5"></div>
-                    <div className="card6"></div>
-                    <div className="card7"></div>
-                    <div className="card8"></div>
+                    <div className="card1">
+                    <a href="/">
+                            <h2>Coding and Programming</h2>
+                            <p className="cardText">
+                            You're a click away from the best Developers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card2">
+                        <a href="/">
+                            <h2>Graphics and Design</h2>
+                            <p className="cardText">
+                            You're a click away from the best Designers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card3">
+                        <a href="/">
+                            <h2>Video Editing and Animation</h2>
+                            <p className="cardText">
+                            You're a click away from the best Video Editors and Animators
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card4">
+                        <a href="/">
+                            <h2>Arts and Drawing</h2>
+                            <p className="cardText">
+                            You're a click away from the best Artist
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card5">
+                        <a href="/">
+                            <h2>Music and Melody</h2>
+                            <p className="cardText">
+                            You're a click away from the best musicians and Producers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card6">
+                        <a href="/">
+                            <h2>Modelling</h2>
+                            <p className="cardText">
+                            You're a click away from the best Modellers
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card7">
+                        <a href="/">
+                            <h2>Architectural designs</h2>
+                            <p className="cardText">
+                                You're a click away from the best Archtects
+                            </p>
+                        </a>
+                    </div>
+                    <div className="card8">
+                        <a href="/">
+                            <h2>Influencing</h2>
+                            <p className="cardText">
+                            You're a click away from the best Influencers
+                            </p>
+                        </a>
+                    </div>
                 </span>
             </div>
         </div>
