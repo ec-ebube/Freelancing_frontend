653f1a50d0bff07adfca60c16b868e9af3324521
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-07-17 17:10:09 +0100

    17 July 2023

--- src/Components/Home/HomeOne.js
+++ src/Components/Home/HomeOne.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import BusiClass from "../../Assets/Images/business.jpg"
-import arrow_forward from "../../Assets/Icons/arrow_forward_ios_FILL0_wght400_GRAD0_opsz48.svg"
+// import arrow_forward from "../../Assets/Icons/arrow_forward_ios_FILL0_wght400_GRAD0_opsz48.svg"
 
 function HomeOne() {
   return (
@@ -20,7 +20,12 @@ function HomeOne() {
             <img src={BusiClass} alt="Boy" />
           </div> */}
         </div>
-        <button className='startbtn' >Start Now<img src={arrow_forward} alt="" className='arrowIcon'/></button>
+        <button className='startbtn' >
+          Start Now
+          <svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="48">
+            <path d="m304-82-56-57 343-343-343-343 56-57 400 400L304-82Z" />
+          </svg>
+        </button>
       </div>
     </div>
   )
