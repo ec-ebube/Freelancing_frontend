c29ecf1434d6a7cc5e47383dc99bde405dbc1c94
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-07-18 14:42:53 +0100

    18 July 2023  14:42

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './Styles/App.css';
 import './Styles/Headers.css';
 import './Styles/Home.css';
+import './Styles/Loading.css'
 import Header from './Neccessary/Header';
-// import HomeOne from './Components/HomeOne';
-// import HomeTwo from './Components/HomeTwo';
 import Home from './Components/Home';
-// import DropCatergory from './Neccessary/DropCatergory';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <HomeOne />
-      <HomeTwo /> */}
       <Home />
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeOne from './Home/HomeOne';
 import HomeTwo from './Home/HomeTwo';
+// import Loading from '../Neccessary/Loading';
 
 function Home() {
   return (
     <div>
       <HomeOne />
       <HomeTwo />
+      {/* <Loading /> */}
     </div>
   )
 }
