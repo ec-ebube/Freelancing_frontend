d220c3eccc8a8fcd2ff6f5a8587a2037cbefa31e
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-07-18 02:44:59 +0100

    18 July 2023, 02:44

--- src/Components/Home/HomeOne.js
+++ src/Components/Home/HomeOne.js
@@ -16,16 +16,20 @@ function HomeOne() {
             </div>
             Today
           </div>
-          {/* <div className="desImg">
-            <img src={BusiClass} alt="Boy" />
-          </div> */}
+          <div className="welcomeDiv">
+            <p className="welcomeText">
+            Trusted and used by Millions on both ends of every service Industry
+            </p>
+          </div>
         </div>
-        <button className='startbtn' >
-          <svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="48" className='arrowIcon'>
-            <path d="m304-82-56-57 343-343-343-343 56-57 400 400L304-82Z" />
-          </svg>
-          Start Now
-        </button>
+        <div className="forbtn">
+          <button className='startbtn' >
+            <svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 -960 960 960" width="48" className='arrowIcon'>
+              <path d="m304-82-56-57 343-343-343-343 56-57 400 400L304-82Z" />
+            </svg>
+            Start Now
+          </button>
+        </div>
       </div>
     </div>
   )
