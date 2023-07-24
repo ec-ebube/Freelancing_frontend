0760f3e5711765a4711065e8fc8730b8bbca5b13
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-07-24 12:42:31 +0100

    21 Mon July 2023

--- src/EndPoints.js
+++ src/EndPoints.js
@@ -1,3 +1,4 @@
 const baseUrl = "https://localhost:7229/api/";
 export const PortfoliosURL = baseUrl + "portfolio/getall/";
+export const ProjectsURL = baseUrl + "project/getall/";
 export const singlePortfolio = baseUrl + "portfolio/get/"
