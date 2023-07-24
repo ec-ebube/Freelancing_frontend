f1f377025ceebb399e2964513ecd05ea3113ea15
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-07-24 18:11:54 +0100

    23 July 2023

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { singlePortfolio } from '../../EndPoints';
 import useFetch from '../../Hooks/useFetch';
 import { useParams } from 'react-router-dom';
+import Loading from '../../Neccessary/Loading';
 
 function Portfolio() {
 
@@ -14,9 +15,19 @@ function Portfolio() {
 
   return (
     <div>
-      <div>
-        <img src={require(`../../../../Backend/${PortfolioData.ProfilePath}`)} alt="" />
-      </div>
+      {isLoading && <div className='contLoad'><Loading /></div>}
+      {error && <div>{Error}</div>}
+      {PortfolioData && <div>
+        <div>
+          <div>
+            <img src={require(`../../../../Backend/${PortfolioData.ProfilePath}`)} alt=""></img>
+          </div>
+          <div>
+            <h3>{PortfolioData.FirstName} {PortfolioData.LastName}</h3>
+            <h5>@{PortfolioData.UserName}</h5>
+          </div>
+        </div>
+      </div>}
     </div>
   )
 }
