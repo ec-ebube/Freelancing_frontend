d5d01301811e94341c0e3ac67b4314da2fa1bcf9
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-07-26 10:21:00 +0100

    Wed 26 July 2023

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ProjectsURL, singlePortfolio } from '../../EndPoints';
 import useFetch from '../../Hooks/useFetch';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Loading from '../../Neccessary/Loading';
 
 function Portfolio() {
@@ -22,38 +22,56 @@ function Portfolio() {
     })
   }
   console.log(PortProject);
-  
+
 
   return (
     <div className="PortBody">
       {isLoading && <div className='contLoad'><Loading /></div>}
       {error && <div>{Error}</div>}
       {PortfolioData && <div className="portData">
-        <div>
-          <div>
-            <img src={require(`../../../../Backend/${PortfolioData.ProfilePath}`)} alt=""></img>
+        <div className='port1sect'>
+          <div className='PortImgDiv'>
+            <img src={require(`../../../../Backend/${PortfolioData.ProfilePath}`)} alt="" className='profilePic'></img>
           </div>
-          <div>
-            <h3>{PortfolioData.FirstName} {PortfolioData.LastName}</h3>
-            <h5>@{PortfolioData.UserName}</h5>
+          <div className="portContext">
+            <div className='portText'>
+              <h3 className='portName'>{PortfolioData.FirstName} {PortfolioData.LastName}</h3>
+              <h5 className='portUser'>@{PortfolioData.UserName}</h5>
+            </div>
+            <div className='portOthers'>
+              <p>Bio: {PortfolioData.Bio}</p>
+              <p>Category: {PortfolioData.Category}</p>
+              <p>Skill: {PortfolioData.Skill}</p>
+              <p>Email : {PortfolioData.Email}</p>
+            </div>
           </div>
-        </div>
-        <div>
-          <p>Bio: {PortfolioData.Bio}</p>
-          <p>Category: {PortfolioData.Category}</p>
-          <p>Skill: {PortfolioData.Skill}</p>
         </div>
         <div>
           {PortProject && (
             PortProject.map((Dprojects, Index) => {
               return (
-                <div>
-                  <h5>{Dprojects.Title}</h5>
-                </div>
+                <Link to={`/`} className='catLinkdiv' key={Index}>
+
+                  <div className="catData">
+                    {((Dprojects.ProjectImagePath != null) && (Dprojects.ProjectVideoPath == null)) && <div>
+                      <img src={require(`../../../../Backend/${Dprojects.ProjectImagePath}`)} alt="" className='catImage' />
+                    </div>}
+                    {((Dprojects.ProjectImagePath == null) && (Dprojects.ProjectVideoPath != null)) && <div>
+                      {/* <img src={require(`../../../../Backend/${Dprojects.ProjectVideoPath}`)} alt="" className='catImage' /> */}
+                    <video src={require(`../../../../Backend/${Dprojects.ProjectVideoPath}`)} alt="" className='catImage' controls ></video>
+                    </div>}
+                    <div className="catDetails">
+
+                    </div>
+                  </div>
+                </Link>
               );
             })
           )
           }
+          {(PortProject.length === 0) && <div>
+            <h3>No Projects Uploaded Yet</h3>
+          </div>}
         </div>
       </div>}
     </div>
