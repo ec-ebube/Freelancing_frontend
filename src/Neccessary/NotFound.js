65509214157275ca3e0554afeb578b2cdd86bdb3
Emmanuel Chimdiebube <onweemmanuelchimdiebube@gmail.com>
2023-08-01 10:31:28 +0100

    Tue 1 August 2023

--- src/Neccessary/NotFound.js
+++ src/Neccessary/NotFound.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import '../Styles/NotFound.css'; 
 
 const NotFound = () => {
-    const [animate, setAnimate] = useState(false);
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
     const animationInterval = setInterval(() => {
       setAnimate(true);
-      setTimeout(() => setAnimate(false), 1000); // Duration of each animation shake
-    }, 10000); // 10 seconds delay before each animation starts
+      setTimeout(() => setAnimate(false), 1000); // Slight delay before scaling down
+    }, 4000); // 10 seconds delay before each animation starts
 
     return () => {
       clearInterval(animationInterval);
@@ -16,12 +16,11 @@ const NotFound = () => {
   }, []);
 
   return (
-    <div className={`not-found-page ${animate ? 'shake' : ''}`}>
-      <h1>404 Page Not Found</h1>
+    <div className="not-found-page">
+      <h1 className={animate ? 'shake' : ''}>404 Page Not Found</h1>
       {/* Add any additional content you want on your 404 page */}
     </div>
   );
 };
 
- 
 export default NotFound;
